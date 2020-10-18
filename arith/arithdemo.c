ff61bfbb845e11e7a5b3224c6d1c4c1ad5c32531
Rinku Chavhan <rinkuchavhan8@gmail.com>
2020-10-19 02:37:52 +0530

    Creating libraries demo

---
+++ arith/arithdemo.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int add(int, int);//declaration
+int sub(int, int);
+
+int main()
+{
+    printf("Addition: %d\n",add(10,20));
+    printf("Subtraction: %d\n",sub(50,20));
+
+    return 0;
+
+}
---
+++ arith/pune/main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include "arith.h"
+
+int main()
+{
+    printf("FRiend Add: %d\n",add(60,50));
+    return 0;
+}
+
